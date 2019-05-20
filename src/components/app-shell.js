a7a4a4fc06f47fee2c9eec2125945cc9e323921d
Jacob Casado <jacob.casado@soprasteria.com>
2019-05-20 16:25:35 +0200

    Added components

--- src/components/app-shell.js
+++ src/components/app-shell.js
@@ -1,7 +1,18 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 
 export class AppShell extends LitElement {
 
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+
+      p {
+        color: blue;
+      }
+    `;
+  }
 
   render() {
     return html`
